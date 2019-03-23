7f0a775819f662c3005f998ed32746872728f33d
jkazuo55 <jkazuo555@gmail.com>
2019-03-23 21:40:31 +0000

    quitando register de la pagina de inicio

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,17 @@ Route::get('admin', function(){
     return view('admin.dashboard');
 });
 
-Route::auth();
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes... Ocultamos formulario register para que no aparesca a inicio
+// Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+// Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
