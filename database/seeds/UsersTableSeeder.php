cc5d4848263bc48537a7e457624b6a15e3aaaf5c
jkazuo55 <jkazuo555@gmail.com>
2019-03-25 00:07:55 +0000

    agregando nuevo usuario en seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,12 +18,22 @@ class UsersTableSeeder extends Seeder
         //creamos un usuario
         $user = new User;
         $user->name = 'julian';
-            $user->apPat = 'huarachi';
-            $user->apMat = 'mamani';
-            $user->ci = '7095167';
-            $user->sis = '201100964';
-            $user->email = 'jkazuo55@gmail.com';
-            $user->password = bcrypt('holamundo');
-            $user->save();
+        $user->apPat = 'huarachi';
+        $user->apMat = 'mamani';
+        $user->ci = '7095167';
+        $user->sis = '201100964';
+        $user->email = 'jkazuo55@gmail.com';
+        $user->password = bcrypt('holamundo');
+        $user->save();
+
+        $user = new User;
+        $user->name = 'maria';
+        $user->apPat = 'huayllucu';
+        $user->apMat = 'escobar';
+        $user->ci = '7095167';
+        $user->sis = '201502152';
+        $user->email = 'maria@gmail.com';
+        $user->password = bcrypt('holamundo');
+        $user->save();
     }
 }
